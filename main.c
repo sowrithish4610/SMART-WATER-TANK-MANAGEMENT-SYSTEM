6718eac596bd60bdf3d14ac55db055cdb308a8b2
sowrithish4610 <sowrithish.46@gmail.com>
2026-09-14 23:07:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -57,12 +57,18 @@ int main(){
         actual_temp  = smart_tank_adc_temp();
         distance_cm  = ultrasonic_distance_cm();
 
-        s     = i2c_read(0xD1, 0x00); delay_ms(5);
-        m     = i2c_read(0xD1, 0x01); delay_ms(5);
-        h     = i2c_read(0xD1, 0x02); delay_ms(5);
-        date  = i2c_read(0xD1, 0x04); delay_ms(5);
-        month = i2c_read(0xD1, 0x05); delay_ms(5);
-        year  = i2c_read(0xD1, 0x06); delay_ms(5);
+        s     = i2c_read(0xD1, 0x00); 
+        delay_ms(5);
+        m     = i2c_read(0xD1, 0x01); 
+        delay_ms(5);
+        h     = i2c_read(0xD1, 0x02); 
+        delay_ms(5);
+        date  = i2c_read(0xD1, 0x04); 
+        delay_ms(5);
+        month = i2c_read(0xD1, 0x05); 
+        delay_ms(5);
+        year  = i2c_read(0xD1, 0x06); 
+        delay_ms(5);
 
         uart0_tx_string("Water Level: ");
         uart0_integer(water_level);
@@ -79,14 +85,20 @@ int main(){
         uart0_tx_string(" C\r\n");
 
         uart0_tx_string("Date: ");
-        uart0_time_div(date); uart0_tx('/');
-        uart0_time_div(month); uart0_tx_string("/20");
-        uart0_time_div(year); uart0_tx_string("\r\n");
+        uart0_time_div(date); 
+        uart0_tx('/');
+        uart0_time_div(month); 
+        uart0_tx_string("/20");
+        uart0_time_div(year); 
+        uart0_tx_string("\r\n");
 
         uart0_tx_string("Current Time: ");
-        uart0_time_div(h); uart0_tx(':');
-        uart0_time_div(m); uart0_tx(':');
-        uart0_time_div(s); uart0_tx_string("\r\n");
+        uart0_time_div(h); 
+        uart0_tx(':');
+        uart0_time_div(m); 
+        uart0_tx(':');
+        uart0_time_div(s); 
+        uart0_tx_string("\r\n");
 
         system_pump_control(water_level);
 
@@ -99,9 +111,15 @@ int main(){
         mem_temp  = eeprom_read_word(0xA1, 0x20);
 
         uart0_tx_string("[ EEPROM READBACK ]\r\n");
-        uart0_tx_string("Saved Water : "); uart0_integer(mem_water); uart0_tx_string(" %\r\n");
-        uart0_tx_string("Saved Dist  : "); uart0_integer(mem_dist);  uart0_tx_string(" cm\r\n");
-        uart0_tx_string("Saved Temp  : "); uart0_integer(mem_temp);  uart0_tx_string(" C\r\n");
+        uart0_tx_string("Saved Water : "); 
+        uart0_integer(mem_water); 
+        uart0_tx_string(" %\r\n");
+        uart0_tx_string("Saved Dist  : "); 
+        uart0_integer(mem_dist);  
+        uart0_tx_string(" cm\r\n");
+        uart0_tx_string("Saved Temp  : "); 
+        uart0_integer(mem_temp);  
+        uart0_tx_string(" C\r\n");
 
         uart0_tx_string("----------------------\r\n");
 
